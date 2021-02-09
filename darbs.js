d9c7638ba18811ebfaee31a5fe08aaf458335f88
mikek6110 <73471001+mikek6110@users.noreply.github.com>
2021-02-09 12:25:11 +0200

    Pievienoju jaunu uzdevumu

--- darbs.js
+++ darbs.js
@@ -2,6 +2,11 @@ function uzd1(){
     window.location.replace("lapa1.html");
 }
 
+function uzd2(){
+    window.location.replace("lapa2.html");
+}
+
+
 function home(){
     window.location.replace("index.html");
 }
